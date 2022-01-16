01595576ddcf07f07a04c66f15c1510e452771e3
hoangdai-git <76163124+hoangdai-git@users.noreply.github.com>
2022-01-16 17:28:26 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState, createContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import Home from "./pages/home/Home";
+import Home from "./pages/Home/Home";
 import ProductDetail from "./pages/ProductDetail";
 import NavBar from "./components/NavBar";
 import Products from "./pages/Products";
