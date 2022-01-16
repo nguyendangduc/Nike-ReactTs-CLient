4e2a22752f53b57692282768f8f92d0f6fb053ff
hoangdai-git <76163124+hoangdai-git@users.noreply.github.com>
2022-01-16 15:45:02 +0700

    reponsive home

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -181,64 +181,66 @@ const NavBar: React.FC<Props> = ({
               </li>
             </ul>
 
-            {isAuth ? (
-              <>
-                <Link to="/profile">
-                  <img
-                    src={dataUser.avatar ? dataUser.avatar : URL_AVATAR}
-                    alt="avatar"
-                    style={avtCss}
-                  />
-                </Link>
-                <span
-                  className="user-logout"
+            <div className="d-flex">
+              {isAuth ? (
+                <>
+                  <Link to="/profile">
+                    <img
+                      src={dataUser.avatar ? dataUser.avatar : URL_AVATAR}
+                      alt="avatar"
+                      style={avtCss}
+                    />
+                  </Link>
+                  <span
+                    className="user-logout"
+                    onClick={() => {
+                      dispatch(logoutSuccess());
+                    }}
+                  >
+                    Log out
+                  </span>
+                  {isAdmin ? (
+                    <Link
+                      to="/admin"
+                      onClick={() => {
+                        setToDashBoard(true);
+                      }}
+                      className="ms-3"
+                    >
+                      <b>Dashboard</b>
+                    </Link>
+                  ) : null}
+                </>
+              ) : (
+                <svg
+                  aria-hidden="true"
+                  data-var="glyph"
+                  fill="#111"
+                  height="30px"
+                  width="30px"
+                  viewBox="0 0 24 24"
                   onClick={() => {
-                    dispatch(logoutSuccess());
+                    history.push("/login");
                   }}
                 >
-                  Log out
-                </span>
-                {isAdmin ? (
-                  <Link
-                    to="/admin"
-                    onClick={() => {
-                      setToDashBoard(true);
-                    }}
-                    className="ms-3"
-                  >
-                    <b>Dashboard</b>
-                  </Link>
-                ) : null}
-              </>
-            ) : (
-              <svg
-                aria-hidden="true"
-                data-var="glyph"
-                fill="#111"
-                height="30px"
-                width="30px"
-                viewBox="0 0 24 24"
-                onClick={() => {
-                  history.push("/login");
-                }}
-              >
-                <path d="M16.44 11A5.94 5.94 0 0 0 18 7 6 6 0 0 0 6 7a5.94 5.94 0 0 0 1.56 4A5 5 0 0 0 3 16v5a1 1 0 0 0 2 0v-5a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v5a1 1 0 0 0 2 0v-5a5 5 0 0 0-4.56-5zM8 7a4 4 0 1 1 4 4 4 4 0 0 1-4-4z"></path>
-              </svg>
-            )}
+                  <path d="M16.44 11A5.94 5.94 0 0 0 18 7 6 6 0 0 0 6 7a5.94 5.94 0 0 0 1.56 4A5 5 0 0 0 3 16v5a1 1 0 0 0 2 0v-5a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v5a1 1 0 0 0 2 0v-5a5 5 0 0 0-4.56-5zM8 7a4 4 0 1 1 4 4 4 4 0 0 1-4-4z"></path>
+                </svg>
+              )}
 
-            <Link to="/cart" className="cart-icon-wrapper">
-              <svg
-                width="30px"
-                height="30px"
-                fill="#111"
-                viewBox="0 0 24 24"
-                className="ms-3 cart-icon"
-              >
-                <path d="M16 7a1 1 0 0 1-1-1V3H9v3a1 1 0 0 1-2 0V3a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v3a1 1 0 0 1-1 1z"></path>
-                <path d="M20 5H4a2 2 0 0 0-2 2v13a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a2 2 0 0 0-2-2zm0 15a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V7h16z"></path>
-              </svg>
-              <span className="cart-quantity">{itemsInCart.length}</span>
-            </Link>
+              <Link to="/cart" className="cart-icon-wrapper">
+                <svg
+                  width="30px"
+                  height="30px"
+                  fill="#111"
+                  viewBox="0 0 24 24"
+                  className="ms-3 cart-icon"
+                >
+                  <path d="M16 7a1 1 0 0 1-1-1V3H9v3a1 1 0 0 1-2 0V3a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v3a1 1 0 0 1-1 1z"></path>
+                  <path d="M20 5H4a2 2 0 0 0-2 2v13a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a2 2 0 0 0-2-2zm0 15a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V7h16z"></path>
+                </svg>
+                <span className="cart-quantity">{itemsInCart.length}</span>
+              </Link>
+            </div>
           </div>
         </div>
       </nav>
